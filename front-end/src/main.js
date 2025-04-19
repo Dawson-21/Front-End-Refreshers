2c8b1126a9f5d7ba2d6c79bb729e593477f24670
Dawson Adams <dawson.adams121@gmail.com>
2025-04-18 19:06:08 -0500

    Begin structuring the home page

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,17 +1,26 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+import javascriptLogo from '/javascript.svg'
+import htmlLogo from '/html-5.svg'
+import cssLogo from '/css-3.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    <h1>Welcome to my Front End Refresher Library!</h1>
+    <a href="#" target="_blank">
+      <img src="${htmlLogo}" class="logo" alt="HTML logo" />
+    </a>
+    <a href="#" target="_blank">
+      <img src="${cssLogo}" class="logo css" alt="CSS logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo vite" alt="CSS logo" />
+    </a>
+    <h2>Click a language to get started!</h2>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
